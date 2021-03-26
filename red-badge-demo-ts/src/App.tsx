313d205d307510eb2d4eff7df2b90849328f3078
Ty Ramsey <tydram@gmail.com>
2021-03-25 21:34:30 -0400

    inclass fetch function to class challenge

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ClassFetchOnload from "./components/ClassFetchOnload";
 import FunctionFetchOnload from "./components/FunctionFetchOnload";
 
 function App() {
   return (
     <div>
       <FunctionFetchOnload />
+      <ClassFetchOnload />
     </div>
   );
 }
